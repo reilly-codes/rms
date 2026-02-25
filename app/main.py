15a4b47efa089229deb9dc8a0edeb5ce4ad35ec0
Reilly Oduory <jroduory@gmail.com>
2026-02-25 12:49:00 +0100

    added netlify app to origins

--- app/main.py
+++ app/main.py
@@ -27,7 +27,8 @@ app = FastAPI(lifespan=lifespan)
 origins = [
     "http://localhost:5173",
     "http://localhost:8080",
-    "http://127.0.0.1:8080"
+    "http://127.0.0.1:8080",
+    "rentmgt.netlify.com"
 ]
 
 app.add_middleware(
